55c6310f2307233b65fd0c9b97997fcbfce4f090
Ibrahim <ibrahimmiishaq@gmail.com>
2025-07-29 15:45:24 +0400

    Remove unused InteractWithThumbnail coroutine
    Deleted the InteractWithThumbnail coroutine and its invocation as it is no longer used in ThumbnailInteraction. This cleans up the script and removes dead code.

--- Assets/Scripts/ThumbnailInteraction.cs
+++ Assets/Scripts/ThumbnailInteraction.cs
@@ -14,29 +14,10 @@ public class ThumbnailInteraction : MonoBehaviour
                 {
                     var obj = hit.collider.gameObject;
                     // Start centering process
-                    StartCoroutine(InteractWithThumbnail(obj));
                 }
             }
         }
     }
 
-    IEnumerator InteractWithThumbnail(GameObject obj)
-    {
-        Vector3 targetPos = obj.transform.position + obj.transform.forward * 1.5f;
-        Quaternion targetRot = Quaternion.LookRotation(obj.transform.position - Camera.main.transform.position);
-        float t = 0f;
-        float duration = 1f;
-        Vector3 startPos = transform.position;
-        Quaternion startRot = transform.rotation;
-        while (t < 1f)
-        {
-            t += Time.deltaTime / duration;
-            transform.position = Vector3.Lerp(startPos, targetPos, t);
-            transform.rotation = Quaternion.Slerp(startRot, targetRot, t);
-            yield return null;
-        }
-        // Show UI panel
-        UIManager.Instance.ShowProjectInfo(obj);
-    }
 }
 
